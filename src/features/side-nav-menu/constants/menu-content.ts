0f1802dd5b6c294d46c174ae8f454ed5043f76a5
v.shtyl <vlad751392@gmail.com>
2023-08-14 14:28:39 +0300

    fix

--- src/features/side-nav-menu/constants/menu-content.ts
+++ src/features/side-nav-menu/constants/menu-content.ts
@@ -1,19 +1,25 @@
 import { AppRoutes } from '@shared/constants';
 
-import { ReactComponent as DashboardIcon } from '@svg/dashboard.svg';
-import { ReactComponent as UserIcon } from '@svg/user.svg';
+import { ReactComponent as CalendarIcon } from '@svg/calendar.svg';
+import { ReactComponent as EventsIcon } from '@svg/events.svg';
+import { ReactComponent as DashboardIcon } from '@svg/users.svg';
 
 import { MenuItem } from '../types/menu.interfaces';
 
 export const MENU_CONTENT: MenuItem[] = [
   {
-    path: AppRoutes.DASHBOARD,
+    path: AppRoutes.USERS,
     icon: DashboardIcon,
-    text: 'Dashboard'
+    text: 'Users'
   },
   {
-    path: AppRoutes.USER,
-    icon: UserIcon,
-    text: 'User'
+    path: AppRoutes.REQUESTS,
+    icon: CalendarIcon,
+    text: 'Requests'
+  },
+  {
+    path: AppRoutes.EVENTS,
+    icon: EventsIcon,
+    text: 'Events'
   }
 ];
